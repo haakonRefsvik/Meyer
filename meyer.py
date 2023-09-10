f32d3e78a8aa89f03fe334b86655156ac928549d
Håkon Bjørgen Refsvik <haakoref@ulrik.uio.no>
2023-09-10 14:41:34 +0200

    Første commit

---
+++ meyer.py
@@ -0,0 +1,121 @@
+from typing import Any
+import pygame
+from sys import exit
+import random
+from pygame.sprite import AbstractGroup
+
+# TODO
+# Få cup-klassen til å si ifra til dice klassen at den er ferdig med å rise 
+
+pygame.init()
+
+bredde = 500
+hoyde = 800
+cupXSpeed = 10
+cupYSpeed = 2.1
+moveCup = True
+
+screen = pygame.display.set_mode((bredde, hoyde))
+pygame.display.set_caption("Meyer")
+clock = pygame.time.Clock()
+
+kopp = pygame.image.load("grafikk/kopper.png").convert_alpha()
+kopp = pygame.transform.rotozoom(kopp, 0, 0.3)
+cupX = 200
+cupY = 90
+
+class Cup(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, size: int):
+        super().__init__()
+        self.image = pygame.image.load("grafikk/kopper.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, size)
+        self.rect = self.image.get_rect(midbottom = (x, y))
+        self.xMiddle = x
+        self.yMiddle = y
+        self.isPressed = False
+
+    def followCursor(self):
+        cursor = pygame.mouse.get_pos()
+        movementSpeed = 20
+        smoothenes = 80
+
+        xDiff = cursor[0] - self.rect.center[0]
+        yDiff = cursor[1] - self.rect.center[1]
+
+        if(not self.isPressed):
+            xDiff = self.xMiddle - self.rect.center[0]
+            yDiff = self.yMiddle - self.rect.midbottom[1]
+
+            if(xDiff != 0):
+                self.rect.x += movementSpeed * (xDiff/smoothenes)
+
+            if(yDiff != 0):
+                self.rect.y += movementSpeed * (yDiff/smoothenes)
+            
+            return
+        
+        if(xDiff < -50 or xDiff > 50):
+            self.rect.x += movementSpeed * (xDiff/smoothenes)
+
+        if(yDiff < -50 or yDiff > 50):
+            self.rect.y += movementSpeed * (yDiff/smoothenes)
+
+    def detectPress(self):
+        x = pygame.mouse.get_pos()[0]
+        y = pygame.mouse.get_pos()[1]
+
+        if(event.type == pygame.MOUSEBUTTONDOWN):
+            self.isPressed = True
+
+        elif(event.type == pygame.MOUSEBUTTONUP):
+            self.isPressed = False
+
+    def update(self):
+        self.followCursor()
+        self.detectPress()
+
+class Dice(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, size):
+        super().__init__()
+        self.image = pygame.image.load("grafikk/dice_1.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, size)
+        self.rect = self.image.get_rect(midbottom = (x, y))
+        self.xMiddle = x
+        self.yMiddle = y
+        self.isPressed = False
+    
+    def show(self):
+        print("flytter inn")
+
+    def update(self):
+        self.show()        
+
+cup = pygame.sprite.GroupSingle()
+cup.add(Cup(bredde/2, hoyde*0.9, 0.2))
+
+dice = pygame.sprite.GroupSingle()
+dice.add(Dice(bredde/2, hoyde*0.9, 0.1))
+
+bakgrunn = pygame.image.load("grafikk/bakgrunn.png").convert_alpha()
+bakgrunn = pygame.transform.rotozoom(bakgrunn, 0, 0.5)
+
+tekst = pygame.font.Font("font/banger.ttf", 50)
+tekst = tekst.render("MEYER", False, "White")
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(bakgrunn,(-30,0))
+    screen.blit(tekst, (150, 50))
+
+    cup.draw(screen)
+    cup.update()
+    dice.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
+
+
